b8b0d4eab4366dff111fe4b95c6e3c7801dc3c6f
=m4ttper0 <=m4ttper0@gmail.com>
2024-08-07 10:21:25 -0400

    major overhaul, created dynamic menu options in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Header() {
-        return (
-            <header id="headerComponent">
-                <div id="headerLogo">MP</div>
-                <div className="headerText"></div>
-            </header>
-        )
+const Header = () => {
+
+    const [menuOptions, setMenuOptions] = useState(window.innerWidth > 480)
+
+    const handleMenuOptions = () => {
+        setMenuOptions(window.innerWidth > 480)
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", handleMenuOptions)
+        return () => { window.removeEventListener("resize", handleMenuOptions) }
+    })
+
+    return (
+        <header id="headerComponent">
+        <div id="headerLogo">
+            matt pero
+        </div>
+        {
+            menuOptions ? (<>
+                            <div className="headerTab">
+                                About
+                            </div>
+                            <div className="headerTab">
+                                Experience
+                            </div>
+                            <div className="headerTab">
+                                Contact
+                            </div>
+                          </>) 
+                        : (<div id="headerTabDropdown" className="headerTab">
+                                &#9776;
+                          </div>)
+        }
+
+    </header>
+    )
 }
+
+export default Header;
