410c457d0706002db57628b6d573c355b4e4ac7d
mattpero22 <mattpero22@gmail.com>
2024-08-20 10:37:41 -0400

    new portfolio project initiated with MVC architechture

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import '../styles/Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <header id="headerComponent">
+                Welcome to the site
+            </header>
+        )
+    }
+}
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import '../styles/Main.css'
+
+class Main extends Component {
+
+    render() {
+        return (
+            <div id="mainComponent">
+                Welcome to the site
+            </div>
+        )
+    }
+}
+
+export default Main;
