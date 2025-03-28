77c11c892133ea551a182ce7be39221a6382f981
Matt Pero <mattpero22@gmail.com>
2025-03-28 09:57:30 -0400

    small update

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,7 @@ const Landing = () => {
     return (
         <div id="landing">
             <div id="landingName"><strong>Matt Pero</strong></div>
-            <div id="landingTitle">Software engineer</div>
+            <div id="landingTitle">Engineer</div>
         </div>
     )
 }
